025bdf6da8f030f29c1a425b6f7d32a5a78a5acf
Chris kabue <chris.kabue@student.moringaschool.com>
2026-03-02 19:13:17 +0300

    oop-p2-cash-register-lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,57 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+  
+    def __init__(self, discount=0):
+        self._discount = 0
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if isinstance(value, int) and 0 <= value <= 100:
+            self._discount = value
+        else:
+            print("Not valid discount")
+
+   
+
+    def add_item(self, item, price, quantity=1):
+        self.total += price * quantity
+
+        # Add item multiple times
+        for _ in range(quantity):
+            self.items.append(item)
+
+        self.previous_transactions.append(price * quantity)
+
+    
+
+    def apply_discount(self):
+
+        if self.discount == 0:
+            print("There is no discount to apply.")
+            return
+
+        discount_amount = self.total * (self.discount / 100)
+        self.total -= discount_amount
+
+        print(f"After the discount, the total comes to ${int(self.total)}.")
+
+   
+    def void_last_transaction(self):
+
+        if not self.previous_transactions:
+            print("There is no transaction to void.")
+            return
+
+        last_transaction = self.previous_transactions.pop()
+        self.total -= last_transaction
+
+        if self.items:
+            self.items.pop()
+    pass
